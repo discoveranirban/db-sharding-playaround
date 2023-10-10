2639b5a609f487a354083914c060361af9bd1555
ANIRBAN CHAKRABORTY <anirban.chakraborty@cuemath.com>
2023-10-10 13:05:46 +0530

    sharding with postgres

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const {Client} = require('pg');
+var cors = require('cors');
+const crypto = require("crypto");
+const HashRing = require('hashring');
+const hr = new HashRing();
+hr.add("5432");
+hr.add("5433");
+hr.add("5434");
+
+const clients = {
+    "5432" : new Client({
+        "host": "localhost",
+        "port": "5432",
+        "user": "postgres",
+        "password": "password",
+        "database": "postgres"
+    }),
+    "5433" : new Client({
+        "host": "localhost",
+        "port": "5433",
+        "user": "postgres",
+        "password": "password",
+        "database": "postgres"
+    }),
+    "5434" : new Client({
+        "host": "localhost",
+        "port": "5434",
+        "user": "postgres",
+        "password": "password",
+        "database": "postgres"
+    })
+}
+
+connect();
+async function connect() {
+    await clients["5432"].connect();
+    await clients["5433"].connect();
+    await clients["5434"].connect();
+}
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/:urlId', async (req, res) => {
+    const urlId = req.params.urlId;
+    const server = hr.get(urlId);
+
+    const result = await clients[server].query("SELECT * FROM URL_TABLE WHERE URL_ID = $1", [urlId]);
+
+    if(result?.rowCount > 0){
+        res.send({
+            "urlId": urlId,
+            "url": result.rows[0],
+            "server": server
+        })
+    } else {
+        res.sendStatus(404);
+    }
+ });
+
+app.post('/', async (req, res) => {
+    const url = req.query.url;
+    const hash = crypto.createHash("sha256").update(url).digest("base64");
+    const urlId = hash.substr(0, 5);
+
+    const server = hr.get(urlId);
+
+    await clients[server].query("INSERT INTO URL_TABLE (URL, URL_ID) VALUES ($1, $2)", [url, urlId]);
+
+    res.send({
+        "urlId": urlId,
+        "url": url,
+        "server": server
+    })
+  });
+  
+app.listen(3005, () => {
+    console.log('Listening on port 3005');
+  });
